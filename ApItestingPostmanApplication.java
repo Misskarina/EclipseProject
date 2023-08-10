547c7d29297dac4e737e9dfa6cbc87265abc4580
Misskarina <126039790+Misskarina@users.noreply.github.com>
2023-08-10 18:07:43 +0530

    Add files via upload

---
+++ ApItestingPostmanApplication.java
@@ -0,0 +1,12 @@
+package com.Demo.APItestingPostman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ApItestingPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApItestingPostmanApplication.class, args);
+	}
+
+}
